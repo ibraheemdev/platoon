e389ca6ae8722840ac290e8fa54bbab1230790d9
Ibraheem Ahmed <ibrah1440@gmail.com>
2021-11-14 22:28:30 -0500

    add more assertion tests

--- tests/assertions.rs
+++ tests/assertions.rs
@@ -1,7 +1,7 @@
 use static_assertions::assert_not_impl_any;
 
 #[test]
-fn assert_not_send() {
+fn assertions() {
     assert_not_impl_any!(platoon::Runtime: Send, Sync);
     assert_not_impl_any!(platoon::net::TcpListener: Send, Sync);
     assert_not_impl_any!(platoon::net::TcpStream: Send, Sync);
@@ -9,8 +9,12 @@ fn assert_not_send() {
     assert_not_impl_any!(platoon::time::Sleep: Send, Sync);
     assert_not_impl_any!(platoon::time::Timeout<()>: Send, Sync);
     assert_not_impl_any!(platoon::task::JoinHandle<()>: Send, Sync);
-
+    assert_not_impl_any!(platoon::sync::Semaphore: Send, Sync);
+    assert_not_impl_any!(platoon::sync::Mutex<()>: Send, Sync);
+    assert_not_impl_any!(platoon::sync::MutexGuard<'_, ()>: Send, Sync);
+    assert_not_impl_any!(platoon::sync::RwLock<()>: Send, Sync);
+    assert_not_impl_any!(platoon::sync::RwLockReadGuard<()>: Send, Sync);
+    assert_not_impl_any!(platoon::sync::RwLockWriteGuard<()>: Send, Sync);
     assert_not_impl_any!(platoon::sync::oneshot::Sender<()>: Send, Sync);
     assert_not_impl_any!(platoon::sync::oneshot::Receiver<()>: Send, Sync);
-    assert_not_impl_any!(platoon::sync::Semaphore: Send, Sync);
 }
